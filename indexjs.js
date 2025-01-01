b60ca07e6475da2a170f1cbc5e8616ea48eef2b7
RIYAD-21 <ertilriyad2103@gmail.com>
2025-01-01 23:40:19 +0100

    Add audio effects to various pages and adjust ingredient limit notification

--- indexjs.js
+++ indexjs.js
@@ -5,9 +5,11 @@ const list = [];
 var v = true;
 const KrabbyPatty = ["beef","Ketchup","mustard","pickel","onion","lettuce","cheese","tomato"];
 var n = KrabbyPatty.length;
+const sound = document.getElementById('AddSound');
+
 
 function addContainer(ingredient) {
-    const triggerImage = document.getElementById(ingredient);
+    
     const newContainer = document.createElement('div');
     if(list.length < 18) {
         if (v) {
@@ -16,10 +18,12 @@ function addContainer(ingredient) {
                 case "meat":
                     newContainer.className = 'beef';
                     list.push("beef");
+                    
                     break;
                 case "cheese":
                     newContainer.className = 'cheese';
                     list.push("cheese");
+                    
                     break;
                 case "tomato":
                     newContainer.className = 'tomato';
@@ -58,33 +62,32 @@ function addContainer(ingredient) {
                     end.style.display = "none";
                     break;
             }
+            
             containerArea.insertBefore(newContainer, containerArea.firstChild);
+            
             newContainer.style.zIndex = z;
             newContainer.style.position = "absolute";
             newContainer.style.top = `${t}px`;
-            t -= 20; 
+            t -= 18;
+            sound.play();
         }
     }else{
         window.alert("You have reached the maximum amount of ingredients");
     }
+    
 }
 
-triggerImage.addEventListener('click', addContainer);
-
 function CheckBurger(){
-  r = list.length;
-  if(r == n){
-      for(i = 0; i < n; i++){
-          if(KrabbyPatty[i] == list[i]){
-              continue;
-          }else{
-            window.open("losePage.html", "_top");
-            break;
-          }
-      }window.open("winPage.html", "_top");
-  }else{
-    window.open("losePage.html", "_top");
-  }
+    t
+            var s = JSON.stringify(list);
+            var p = JSON.stringify(KrabbyPatty);
+              if(s === p){
+                window.open("winPage.html", "_top");
+              }else{
+                window.open("losePage.html", "_top"); 
+                
+              }    
+     
 }   
 
 function endBurger(){
